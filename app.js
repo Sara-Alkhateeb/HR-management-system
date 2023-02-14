cd5c31291125bb9d70f6a2f81acbbeee3a02a4ef
Sara-Alkhateeb <sarajamal11399@yahoo.com>
2023-02-14 15:37:21 +0300

    updated on new branch

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ function EmployeesData(EmployeeID, FullName, Department, Level, img) {
     this.Department = Department;
     this.Level = Level;
     this.img = img;
+    
 }
 
 let DataSam1 = new EmployeesData(1001, "Lana Ali", "Finance", "Senior", "./EXTRA/lana.jpg");
@@ -14,7 +15,7 @@ let DataSam3 = new EmployeesData(1003, "Safi Walid", "Administration", "Mid-Seni
 let DataSam4 = new EmployeesData(1004, "Omar Zaid", "Development", "Senior", "./EXTRA/omar.jpg");
 let DataSam5 = new EmployeesData(1005, "Rana Saleh", "Development", "Junior", "./EXTRA/rana.jpg");
 let DataSam6 = new EmployeesData(1006, "Hadi Ahmad", "Finance", "Mid-Senior", "./EXTRA/hadi.jpg");
-let DataSam7 = new EmployeesData(  1000,"Ghazi Samer","Administration","Senior","./EXTRA/ghazi.jpg");
+let DataSam7 = new EmployeesData(1000, "Ghazi Samer", "Administration", "Senior", "./EXTRA/ghazi.jpg");
 
 
 
@@ -45,26 +46,23 @@ EmployeesData.prototype.Salary = function () {
 
 
     }
-   // return RanSalary;
+    return RanSalary;
 }
 
-/*function RandSalary(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-   
-  }*/
 
 EmployeesData.prototype.NetSalary = function () {
 
     var NetSalary = this.Salary() - (this.Salary() * .075);
     return NetSalary;
 }
-console.log(DataSam1.NetSalary());
+//console.log(DataSam1.NetSalary());
 
-EmployeesData.prototype.render = function () {
+// EmployeesData.prototype.render = function () {
 
-    document.write(`${this.FullName} ${this.NetSalary()}`);
-    document.write("<br>");
-}
+//     document.write(`${this.img} <br> Name:  ${this.FullName} -ID: ${this.id} <br> 
+//     Department:${this.Department} -Level: ${this.Level} <br> ${this.NetSalary}`);
+//     document.write("<br>");
+// }
 
 
 /*DataSam1.render()
@@ -75,28 +73,66 @@ DataSam5.render()
 DataSam6.render()
 DataSam7.render()*/
 
-/*EmployeesId.prototype.ID = function () {
-    min = 1000;
-    max = 9999;
-    var RanId = Math.floor(Math.random() * (max - min + 1)) + min
-    return RanId;
-}*/
+const generatedIds = new Set();
 
-EmployeesId.prototype.ID = function () {
-var IdNumber = Math.floor(1000 + Math.random() * 9000);
-console.log(IdNumber);}
+function generateEmployeeId() {
+    let id;
+    do {
+        id = Math.floor(Math.random() * 9000) + 1000;
+    } while (generatedIds.has(id));
+    generatedIds.add(id);
+    return id.toString();
+}
+
+
 
 
 let NewUser = document.getElementById("NewUser");
 NewUser.addEventListener('submit', addNewUserHandler);
 
-function addNewUserHandler (event) {
+function addNewUserHandler(event) {
     event.preventDefault();
-    console.log(event);
-    let FullName = event.target.name.value;
-    let Department = event.target.Department.checked;
-    let iLevel = event.target.Level.checked;
-    let imgPath = event.target.imgUrl.value;
-    let NewUser = new EmployeesData (EmployeeID, FullName, Department, Level, img);
+    let EmployeeID = generateEmployeeId();
+    let FullName = event.target.Fullname.value;
+    let Department = event.target.Department.value;
+    let Level = event.target.Level.value;
+    let img = event.target.imgUrl.value;
+    let NewUser = new EmployeesData(EmployeeID, FullName, Department, Level, img);
     NewUser.render();
+}
+
+EmployeesData.prototype.render = function () {
+
+    const container = document.getElementById('Userinfo');
+
+    const divEl = document.createElement('div');
+    container.appendChild(divEl);
+    divEl.classList.add("user")
+
+    const imgEl = document.createElement('img');
+    divEl.appendChild(imgEl);
+    imgEl.setAttribute('src', this.img);
+    
+
+    const nameEl = document.createElement('h3');
+    divEl.appendChild(nameEl);
+    nameEl.textContent = `Name: ${this.FullName}`;
+
+    const IdEl = document.createElement('h3');
+    divEl.appendChild(IdEl);
+    IdEl.textContent = `ID: ${this.EmployeeID}`;
+
+    const DepartmentEl = document.createElement('h3');
+    divEl.appendChild(DepartmentEl);
+    DepartmentEl.textContent = `Department: ${this.Department}`;
+
+    const LevelEl = document.createElement('h3');
+    divEl.appendChild(LevelEl);
+    LevelEl.textContent = `Level: ${this.Level}`;
+
+    const SalaryEl = document.createElement('h3');
+    divEl.appendChild(SalaryEl);
+    SalaryEl.textContent = ` ${this.NetSalary()}`;
+
+    console.log(container)
 }
